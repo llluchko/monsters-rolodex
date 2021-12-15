d62d4b408b11b98f408e5ea5f0b5642a25ce75b1
llluchko <l.mladenov21@gmail.com>
2021-12-15 12:09:30 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searhField: '',
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  // e is a function parameter
+  // javasctipt by default doesn't set the context
+  // arrow function automaticly define this
+
+  handleChange = (e) => {
+    this.setState({ searhField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searhField } = this.state;
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    // setState is async - undefined amount of time - javascript doesn't know about it
+    const filterdMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searhField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filterdMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
